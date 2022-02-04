a5f8b885dd8ca083aca9fb691fd256e93e3a1d2c
Soner Sayakci <s.sayakci@shopware.com>
2022-02-04 08:44:13 +0100

    Add a MySQL dump command

---
+++ cmd/project_dump.go
@@ -0,0 +1,75 @@
+package cmd
+
+import (
+	"database/sql"
+	"github.com/doutorfinancas/go-mad/core"
+	"github.com/doutorfinancas/go-mad/database"
+	"github.com/doutorfinancas/go-mad/generator"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+	"io"
+	"os"
+)
+
+var projectDatabaseDumpCmd = &cobra.Command{
+	Use:   "dump [database]",
+	Short: "Dumps the Shopware database",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cobraCmd *cobra.Command, args []string) error {
+		host, _ := cobraCmd.Flags().GetString("host")
+		port, _ := cobraCmd.Flags().GetString("port")
+		username, _ := cobraCmd.Flags().GetString("username")
+		password, _ := cobraCmd.Flags().GetString("password")
+		output, _ := cobraCmd.Flags().GetString("output")
+		clean, _ := cobraCmd.Flags().GetBool("clean")
+
+		cfg := database.NewConfig(username, password, host, port, args[0])
+
+		db, err := sql.Open("mysql", cfg.ConnectionString())
+
+		if err != nil {
+			return err
+		}
+
+		service := generator.NewService()
+		var opt []database.Option
+		opt = append(opt, database.OptionValue("hex-encode", "1"))
+		opt = append(opt, database.OptionValue("set-charset", "utf8mb4"))
+
+		logger, _ := zap.NewProduction()
+		dumper, err := database.NewMySQLDumper(db, logger, service, opt...)
+
+		pConf := core.Rules{Ignore: []string{}, NoData: []string{}, Where: map[string]string{}, Rewrite: map[string]core.Rewrite{}}
+
+		if clean {
+			pConf.NoData = append(pConf.NoData, "cart", "customer_recovery", "dead_message", "enqueue", "elasticsearch_index_task", "log_entry", "message_queue_stats", "notification", "payment_token", "refresh_token", "version", "version_commit", "version_commit_data", "webhook_event_log")
+		}
+
+		dumper.SetSelectMap(pConf.RewriteToMap())
+		dumper.SetWhereMap(pConf.Where)
+		if dErr := dumper.SetFilterMap(pConf.NoData, pConf.Ignore); dErr != nil {
+			return dErr
+		}
+
+		var w io.Writer
+		if w, err = os.Create(output); err != nil {
+			return err
+		}
+
+		if err = dumper.Dump(w); err != nil {
+			return err
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	projectRootCmd.AddCommand(projectDatabaseDumpCmd)
+	projectDatabaseDumpCmd.Flags().String("host", "127.0.0.1", "hostname")
+	projectDatabaseDumpCmd.Flags().String("username", "root", "mysql user")
+	projectDatabaseDumpCmd.Flags().String("password", "root", "mysql password")
+	projectDatabaseDumpCmd.Flags().String("port", "3306", "mysql port")
+	projectDatabaseDumpCmd.Flags().String("output", "dump.sql", "file")
+	projectDatabaseDumpCmd.Flags().Bool("clean", false, "Ignores cart, enqueue, message_queue_stats")
+}
--- go.mod
+++ go.mod
@@ -4,15 +4,19 @@ go 1.17
 
 require (
 	github.com/caarlos0/env/v6 v6.9.1
+	github.com/doutorfinancas/go-mad v0.0.0-20220204093100-b21521bdc4f5
 	github.com/gorilla/schema v1.2.0
 	github.com/manifoldco/promptui v0.9.0
+	github.com/mholt/archiver/v3 v3.5.1
 	github.com/microcosm-cc/bluemonday v1.0.17
 	github.com/olekukonko/tablewriter v0.0.5
 	github.com/otiai10/copy v1.7.0
 	github.com/pkg/errors v0.8.1
+	github.com/schollz/progressbar/v3 v3.8.5
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/cobra v1.3.0
 	github.com/yuin/goldmark v1.3.5
+	go.uber.org/zap v1.20.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
 
@@ -21,28 +25,27 @@ require (
 	github.com/aymerick/douceur v0.2.0 // indirect
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
 	github.com/dsnet/compress v0.0.2-0.20210315054119-f66993602bf5 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/gorilla/css v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/jaswdr/faker v1.9.0 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/klauspost/pgzip v1.2.5 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
-	github.com/mholt/archiver/v3 v3.5.1 // indirect
-	github.com/mholt/archiver/v4 v4.0.0-alpha.3 // indirect
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/nwaples/rardecode v1.1.0 // indirect
-	github.com/nwaples/rardecode/v2 v2.0.0-beta.2 // indirect
 	github.com/pierrec/lz4/v4 v4.1.12 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/schollz/progressbar/v3 v3.8.5 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/therootcompany/xz v1.0.1 // indirect
 	github.com/ulikunitz/xz v0.5.10 // indirect
 	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce // indirect
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
-	golang.org/x/text v0.3.7 // indirect
 )
