052d6aee294dff2240dbac480e1a37542ba88cef
Kurt Inge Smådal <kurtinge@gmail.com>
2022-11-30 00:14:47 +0100

    Cleanup Changelog template

--- cmd/project/project_extension_create.go
+++ cmd/project/project_extension_create.go
@@ -211,18 +211,18 @@ func makeDefaultServices(pluginPath string) error {
 func makeChangelog(pluginPath string) error {
 	changelogContent := `# Changelog
 
-	All notable changes to this project will be documented in this file.
+All notable changes to this project will be documented in this file.
 
-	The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
-	and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-	## [Unreleased]
+## [Unreleased]
 
-	## [1.0.0]
+## [1.0.0]
 
-	### Added
+### Added
 
-	- Initial version
+- Initial version
 `
 
 	changelogFilename := fmt.Sprintf("%s/CHANGELOG_en-GB.md", pluginPath)
