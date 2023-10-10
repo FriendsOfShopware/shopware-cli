de39abcf0100ddbb0ffaedaec805829ad1d69477
Soner Sayakci <s.sayakci@shopware.com>
2023-10-10 13:02:14 +0200

    fix: skip download tests on Nix build

--- internal/phplint/download_test.go
+++ internal/phplint/download_test.go
@@ -2,12 +2,17 @@ package phplint
 
 import (
 	"context"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestDownloadPHPFile(t *testing.T) {
+	if os.Getenv("NIX_CC") != "" {
+		t.Skip("Downloading does not work in Nix build")
+	}
+
 	_, err := findPHPWasmFile(context.Background(), "7.4")
 	assert.NoError(t, err)
 }
--- internal/phplint/lint_test.go
+++ internal/phplint/lint_test.go
@@ -2,12 +2,17 @@ package phplint
 
 import (
 	"context"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestLintTestData(t *testing.T) {
+	if os.Getenv("NIX_CC") != "" {
+		t.Skip("Downloading does not work in Nix build")
+	}
+
 	errors, err := LintFolder(context.Background(), "7.4", "testdata")
 
 	assert.NoError(t, err)
