9fbf7fdb67fcc61e0ae6958b857bb65a75c7a52e
Soner Sayakci <s.sayakci@shopware.com>
2023-11-28 15:00:44 +0100

    fix: add one more test-case for casing

--- internal/esbuild/util_test.go
+++ internal/esbuild/util_test.go
@@ -11,4 +11,5 @@ func TestKebabCase(t *testing.T) {
 	assert.Equal(t, "f-o-o-bar-baz", ToKebabCase("FOOBarBaz"))
 	assert.Equal(t, "frosh-tools", ToKebabCase("FroshTools"))
 	assert.Equal(t, "my-module-name-s-w6", ToKebabCase("MyModuleNameSW6"))
+	assert.Equal(t, "a-i-search", ToKebabCase("AISearch"))
 }
